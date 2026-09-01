00f567b453c650656d509810f7bd808607b8de00
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-09-01 22:55:29 +0500

    added post and get request without redis

--- redis/index.js
+++ redis/index.js
@@ -1,13 +1,40 @@
-import dotenv from "dotenv"
-import express from "express"
-import connectDb from "./src/config/db.js"
-dotenv.config()
+import dotenv from "dotenv";
+import express from "express";
+import connectDb from "./src/config/db.js";
+import userModel from "./src/models/user.mode.js";
+dotenv.config();
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    connectDb()
-    console.log(`Server running on ${port}`)
-})
+//post request
+
+app.post("/create", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await userModel.create({
+    name,
+    email,
+    password,
+  });
+  res.json({
+    message: "User created",
+    user,
+  });
+});
+
+//get request
+
+app.get("/get", async (req, res) => {
+  const user = await userModel.find({});
+  res.json({
+    message: "All users fetched",
+    user,
+  });
+});
+
+app.listen(port, () => {
+  connectDb();
+  console.log(`Server running on ${port}`);
+});
