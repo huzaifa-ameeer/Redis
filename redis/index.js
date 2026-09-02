d46714d41676f243b030b904e863f36649a7fb93
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-09-02 14:57:58 +0500

    added get request with redis

--- redis/index.js
+++ redis/index.js
@@ -2,9 +2,13 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDb from "./src/config/db.js";
 import userModel from "./src/models/user.mode.js";
+import Redis from "ioredis";
 dotenv.config();
 
 const app = express();
+
+const redis = new Redis(process.env.REDIS_URI);
+
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
@@ -13,6 +17,7 @@ const port = process.env.PORT || 3000;
 
 app.post("/create", async (req, res) => {
   const { name, email, password } = req.body;
+  await redis.del("users:all")
   const user = await userModel.create({
     name,
     email,
@@ -27,7 +32,12 @@ app.post("/create", async (req, res) => {
 //get request
 
 app.get("/get", async (req, res) => {
+  const cached = await redis.get("users:all");
+  if (cached) {
+    return res.json(JSON.parse(cached));
+  }
   const user = await userModel.find({});
+  await redis.set("users:all", JSON.stringify(user));
   res.json({
     message: "All users fetched",
     user,
